36b8b162b513a6ef9b7a1ea808429112d253dc9b
eunyoung kim <snu9451@naver.com>
2021-02-08 13:18:28 +0900

    2차수정완료

--- java210208/src/com/base/HelloWorld.java
+++ java210208/src/com/base/HelloWorld.java
@@ -6,6 +6,7 @@ public class HelloWorld {
 		HelloWorld hw = new HelloWorld();
 		hw.age = 31; //이렇게 다른 값으로 초기화 가능함.
 		System.out.println("당신의 나이는 "+hw.age+"입니다.");
+		System.out.println("2차 수정한 부분입니다.");
 	}
 
 }
