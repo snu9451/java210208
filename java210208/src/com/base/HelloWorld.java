6d92e462ab525f23af961c13ab4866e63c76ab9e
eunyoung kim <snu9451@naver.com>
2021-02-08 13:13:43 +0900

    HelloWorld.java 추가 (EY)

---
+++ java210208/src/com/base/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.base;
+
+public class HelloWorld {
+	int age = 20; //전역변수
+	public static void main(String[] args) {
+		HelloWorld hw = new HelloWorld();
+		hw.age = 31; //이렇게 다른 값으로 초기화 가능함.
+		System.out.println("당신의 나이는 "+hw.age+"입니다.");
+	}
+
+}
